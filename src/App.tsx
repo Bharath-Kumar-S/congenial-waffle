f09af819987ec4f48bbdfaf16b9b0aeefc861cb9
Bharath Kumar <bharathkumarkarthick@gmail.com>
2025-02-18 19:56:56 +0200

    adds count-down

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { CountDown } from "./components/CountDown";
 
 function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <>
+      <CountDown />
+    </>
+  );
 }
 
 export default App;
